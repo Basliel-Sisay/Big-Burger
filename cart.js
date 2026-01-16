8404d93988fafb377a438e29415357475e46a45f
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2026-01-16 11:53:42 +0300

    Added a function that is able to clear everything in the cart

--- cart.js
+++ cart.js
@@ -12,3 +12,17 @@ function add(food , price){
 function remove(foodName){
     cart= cart.filter(foods => foods.name !== foodName);
 }
+function change(food, changes){
+    const foods = cart.find(item => item.name === food);
+    if(foods){
+        foods.amount = food.amount + change;
+        if(foods.amount <1){
+            remove();
+        }
+    }
+}
+function clear(){
+    if(confirm('Are you sure you want to clear the cart?')){
+        cart = [];
+    }
+}
