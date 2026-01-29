248fb09bd2f189ac128baa35eb871a9fb8396a20
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2026-01-29 11:17:15 +0300

    imported react hot toast for the alert message

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import './App.css';
 import App from './App.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Toaster position="top-center" toastOptions={{ duration: 2200 }} />
   </React.StrictMode>
 );
