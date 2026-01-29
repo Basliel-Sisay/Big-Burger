5b255725c3fa05fcd9f131cfc11f8ef46215045f
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2026-01-29 23:40:10 +0300

    Created orders router with POST to place new orders and GET to retrieve all orders

---
+++ Backend/src/routes/orders.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { createOrder, getAllOrders } = require('../controllers/orderController.js');
+router.post('/', createOrder);
+router.get('/', getAllOrders);
+module.exports = router;
